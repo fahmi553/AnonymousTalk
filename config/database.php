5d4afa294bd0c04e2c65261d5d3e68d9bea640a7
fahmi553 <fahmikhalid03@gmail.com>
2026-02-05 18:38:17 +0800

    change database fixing8

--- config/database.php
+++ config/database.php
@@ -45,24 +45,24 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST'),
-            'port' => env('DB_PORT', 3306),
+            'port' => env('DB_PORT'),
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
-            'unix_socket' => null,
+            'unix_socket' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'strict' => true,
             'engine' => null,
 
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
+            'options' => extension_loaded('pdo_mysql') ? [
                 PDO::MYSQL_ATTR_SSL_CA => env(
                     'MYSQL_ATTR_SSL_CA',
                     '/etc/ssl/certs/ca-certificates.crt'
                 ),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ]) : [],
+            ] : [],
         ],
 
         'mariadb' => [
